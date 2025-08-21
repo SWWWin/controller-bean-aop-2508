acc7e8db1309030916bb64ce3e4e17b9ee0c8317
SWWWin <bgpd1258@naver.com>
2025-08-21 12:09:57 +0900

    feat: 기초 셋팅

---
+++ src/main/java/com/back/HomeController.java
@@ -0,0 +1,19 @@
+package com.back;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HomeController {
+
+    private final PersonService personService = new PersonService();
+
+    @GetMapping("/")
+    @ResponseBody
+    public String main() {
+        long count = personService.count();
+        return "home" + count;
+    }
+}
