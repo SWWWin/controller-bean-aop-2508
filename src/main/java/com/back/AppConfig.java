4088d5de468ae3de3d78dcb03397b10952515a0e
SWWWin <bgpd1258@naver.com>
2025-08-21 14:44:03 +0900

    feat: bean 등록 다른 방법

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,9 +1,9 @@
 package com.back;
 
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 
 @Configuration
 public class AppConfig {
@@ -19,14 +19,27 @@ public class AppConfig {
     }
 
     @Bean
-    @Order(2)
     public ApplicationRunner myApplicationRunner() {
-        return new MyApplicationRunner();
+        return new ApplicationRunner() {
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                System.out.println("myApplicationRunner is running");
+            }
+        };
     }
 
     @Bean
-    @Order(1)
-    public ApplicationRunner myApplicationRunnerV2() {
-        return new MyApplicationRunner2();
+    public ApplicationRunner myApplicationRunner2() {
+        return args -> {
+            work1();
+            work2();
+        };
+    }
+
+    private void work2() {
+    }
+
+    private void work1() {
+        
     }
 }
